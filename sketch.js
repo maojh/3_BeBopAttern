34c11c31dda711518b32243144c3114aa6856040
maojh <mjh.ilbasco@gmail.com>
2017-10-17 12:48:37 +0200

    definitive skecth

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@
 //#3 assignement: pattern
 //Matteo Testa
 
-var dista = 30;
+var dista = 35;
 var ray;
 var c;
 
@@ -14,11 +14,13 @@ function setup() {
   noStroke();
   smooth();
   c = 1;
-  frameRate(30);
+  frameRate(1);
 }
 
 var px;
 var m = 0;
+var restart = 100;
+var randy;
 
 function draw() {
    background(0);
@@ -26,14 +28,20 @@ function draw() {
   // rect(0,0,width, height);
 //  var c = int(random(2, 10));
   var noisy = 150;
+  console.log(m);
+
   m++;
-  console.log(c);
-  if( m < 30 ) {
+
+  if (frameCount%3) {
     c+=1;
   }
-  if(c > 30) {
-    c = 1;
+
+  if (m > restart) {
     m = 1;
+  }
+
+  if(c > restart) {
+    c = 1;
   }
 
   var i = 1;
@@ -44,27 +52,28 @@ function draw() {
       if(i%c) {
         noisy = noise(h*w)*235+35;
         px += ray;
+        fill(noisy/8, noisy, noisy/8);
+        ellipse(px, h, ray, ray);
       } else {
+        noisy = noise(h*w)*235+35;
         px -= ray;
+        fill(0, noisy, noisy);
+        push();
+          translate(px, h);
+          rotate(PI/4);
+          rect(ray*.8, -ray/2, ray, ray);
+        pop();
       }
-        // console.log(i);
-        if((w/h)%(frameCount/20)) {
-          fill(noisy/8, noisy, noisy/8);
-          ellipse(px, h, ray, ray);
-          fill(0,0,0, noisy*cos(i));
-          rect(px-ray, h-ray, ray*2, ray*2);
-        } else {
-          push();
-            fill(0, noisy, noisy);
-            translate(px, h-ray/2);
-            rotate(QUARTER_PI);
-            // rect(-ray/2, -ray/2, ray*0.6, ray*0.6);
-            rect( 0, 0, ray*0.5, ray*0.5);
-          pop();
-        }
-
-        i++;
+          i++;
     }
   }
+
+  //frame
+  fill(0);
+  rect(0,0, ray*2, height);
+  rect(0,0, width, ray);
+  rect(0, height-ray, width, ray*2);
+  rect(width-ray, 0, ray*2, height);
+
   //noLoop();
 }
